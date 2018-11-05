579cc031aded65023486da0a00b88df6dc129d0d
summerCat <583188551@qq.com>
2018-11-05 13:50:36 +0800

    使用volatile程序会终止

--- src/threadtest/volatiletest/VolatileDemo.java
+++ src/threadtest/volatiletest/VolatileDemo.java
@@ -5,9 +5,12 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author cxc
  * @date 2018/11/5 13:00
+ * 使用volatile程序会终止
  */
 public class VolatileDemo {
     private static volatile boolean flag = false;
+    //如果不使用volatile 程序不会终止 会有一个副本
+    //private static  boolean flag = false;
 
     public static void main(String[] args) throws InterruptedException{
         Thread t1 = new Thread(){
